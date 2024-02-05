2bda3feacfa85c47f00e3911eafd6b52e89dae27
Zoya <byzyka1986@mail.ru>
2024-02-05 12:55:52 +0300

    home work

---
+++ src/__tests__/test.js
@@ -0,0 +1,12 @@
+import { sortHero } from '../index'
+
+test('sort hero', () => {
+    const heroes = [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+    ]
+const result = sortHero;
+    
+expect(result).toEqual(sortHero);
+}) 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+]
+export const sortHero = heroes.sort((a, b) => b.health - a.health);
+
+
+console.log(sortHero)
